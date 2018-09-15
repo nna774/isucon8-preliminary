d649527080cbf3c4560feeea388aa71fc740ce34
Kugayama Nana <nonamea774@gmail.com>
2018-09-15 19:05:35 +0900

    add authorized keys

--- itamae/roles/base/default.rb
+++ itamae/roles/base/default.rb
@@ -1,1 +1,19 @@
 include_cookbook 'netdata'
+
+%w(
+git
+htop
+).each do |p|
+  package p
+end
+
+directory "#{node[:home]}/.ssh" do
+  owner node[:user]
+  mode '0700'
+end
+
+file "#{node[:home]}/.ssh/authorized_keys" do
+  owner node[:user]
+  mode '0700'
+  content node[:ssh_keys]
+end
