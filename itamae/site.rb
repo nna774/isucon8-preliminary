ab1e7ef713a64451378c3a4952ace2d2be95026c
Kugayama Nana <nonamea774@gmail.com>
2018-09-15 16:55:31 +0900

    add site.rb

---
+++ itamae/site.rb
@@ -0,0 +1,25 @@
+MItamae::RecipeContext.class_eval do
+  ROLES_DIR = File.expand_path("../roles", __FILE__)
+  def include_role(name)
+    recipe_file = File.join(ROLES_DIR, name, "default.rb")
+    include_recipe(recipe_file.to_s)
+  end
+
+  COOKBOOKS_DIR = File.expand_path("../cookbooks", __FILE__)
+  def include_cookbook(name)
+    names = name.split("::")
+    names << "default" if names.length == 1
+    names[-1] += ".rb"
+
+    candidates = [
+      File.join(COOKBOOKS_DIR, *names),
+    ]
+    candidates.each do |candidate|
+      if File.exist?(candidate)
+        include_recipe(candidate)
+        return
+      end
+    end
+    raise "Cookbook #{name} couldn't found"
+  end
+end
