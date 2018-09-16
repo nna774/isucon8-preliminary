d444be32e59666a7fe8e062bb61a2cbe1890738d
Kugayama Nana <nonamea774@gmail.com>
2018-09-16 12:57:46 +0900

    static配信

--- itamae/cookbooks/app/default.rb
+++ itamae/cookbooks/app/default.rb
@@ -12,11 +12,15 @@ file git_ssh do
   content %Q(#!/bin/sh\nexec ssh -oIdentityFile=#{deploy_key} -oStrictHostKeyChecking=no "$@")
 end
 
+directory '/srv/static' do
+  owner 'nginx'
+end
+
 execute 'deploy app' do
   user node[:user]
   command <<"EOC"
 GIT_SSH=#{git_ssh} git pull
-rm #{node[:deploy_to]}/webapp
+cp -rf /home/isucon/torb-tmp/app/webapp/static/* /srv/staticrm #{node[:deploy_to]}/webapp
 ln -sf #{node[:deploy_to]}-tmp/app/webapp #{node[:deploy_to]}/webapp
 EOC
   cwd "#{node[:deploy_to]}-tmp"
@@ -25,9 +29,18 @@ end
 execute 'bundle i' do
   user node[:user]
   command <<"EOC"
-~/local/ruby/bin/bundle install
+~/local/ruby/bin/bundle install --path=vendor/bundle
 EOC
   cwd "#{node[:deploy_to]}/webapp/ruby"
+end
+
+execute 'copy static' do
+  command 'cp -rf /home/isucon/torb-tmp/app/webapp/static/* /srv/static'
+  not_if 'test -e /srv/static/favicon.ico'
+end
+
+execute 'chown static' do
+  command 'chown nginx:nginx -R /srv/static'
 end
 
 execute 'app restart' do
